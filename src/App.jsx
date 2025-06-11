ac1c90beaa5995d380ce6c5586b71cee89ee7d87
Adegboyega Adedamola Isreal <ayomikunadegboyega@gmail.com>
2025-06-11 14:39:16 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,31 @@
-import React from 'react'
-import Navbar from './Navbar'
-import Home from './Home'
-import About from './About'
-import Services from './Services'
-import Projects from './Projects'
-import Pricing from './Pricing'
-import Contact from './Contact'
-import Footer from './Footer'
+import React, { useState } from 'react';
+import Navbar from './Navbar';
+import Home from './Home';
+import About from './About';
+import Services from './Services';
+import Projects from './Projects';
+import Pricing from './Pricing';
+import Contact from './Contact';
+import Footer from './Footer';
+import HireMe from './HireMe'; 
 
 const App = () => {
+  const [isHireMeOpen, setIsHireMeOpen] = useState(false);
+
   return (
     <div>
-      <Navbar />
-      <Home />
+      <Navbar onHireMeClick={() => setIsHireMeOpen(true)} />
+      <Home onHireMeClick={() => setIsHireMeOpen(true)} />
       <About />
       <Services />
       <Projects />
       <Pricing />
       <Contact />
       <Footer />
+
+      <HireMe isOpen={isHireMeOpen} onClose={() => setIsHireMeOpen(false)} />
     </div>
-  )
-}
+  );
+};
 
 export default App;
